2430480ffea007a467c9c582bcf1291abe9d44d0
ivanooi <ivanooi@gmail.com>
2019-04-07 23:04:36 +0800

    邊拉

--- hello-world/src/test_hello_world_git.java
+++ hello-world/src/test_hello_world_git.java
@@ -1,15 +1,19 @@
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.TabFolder;
 
 public class test_hello_world_git extends Shell {
+	private Composite composite;
+	private TabFolder tabFolder;
 
 	/**
 	 * Launch the application.
 	 * @param args
 	 */
 	public static void main(String args[]) {
-		try {
+		try {    
 			Display display = Display.getDefault();
 			test_hello_world_git shell = new test_hello_world_git(display);
 			shell.open();
@@ -40,11 +44,16 @@ public class test_hello_world_git extends Shell {
 		setText("SWT Application");
 		setSize(450, 300);
 
+		
+		composite = new Composite(this, SWT.NONE);
+		composite.setBounds(116, 10, 64, 64);
+		
+		tabFolder = new TabFolder(this, SWT.NONE);
+		tabFolder.setBounds(209, 39, 147, 52);
 	}
 
 	@Override
 	protected void checkSubclass() {
 		// Disable the check that prevents subclassing of SWT components
 	}
-
 }
